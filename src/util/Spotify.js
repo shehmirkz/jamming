2beb9bcfadf22c8a0df84e2ebe7c60b4f3a87daa
Shahmir Hussain <shahmir.hussain.khanzada@gmail.com>
2022-02-28 11:40:50 +0500

    added Spotify Search Request section

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,31 +1,56 @@
-const clientId = '9a264854382a43f2a8efb58ef0403684';
-const redirectUri = 'http://localhost:3000'
+import React from "react";
+import { TrackList } from "../Components/TrackList/TrackList";
+
+const clientId = "9a264854382a43f2a8efb58ef0403684";
+const redirectUri = "http://localhost:3000";
 
 let accessToken;
 
 const Spotify = {
-    getAccessToken () {
-        if(accessToken) {
-            return accessToken;
-
-            const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-            const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
-
-
-            if( accessToken && expiresInMatch) {
-                accessToken = accessTokenMatch[1];
-                const expiresIn = Number(expiresInMatch[1]);
-
-
-                window.setTimeout(() => accessToken = '', expiresIn * 1000);
-                window.history.pushState('Access Token', null, '/');
-                return accessToken;
-            } else {
-                const accessUri = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`
-                window.location = accessUri;
-            }
-        }
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
     }
-}
+
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessToken && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+
+      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+      window.history.pushState("Access Token", null, "/");
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      window.location = accessUrl;
+    }
+  },
+
+  search(term) {
+    const accessToken = Spotify.getAccessToken();
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (!jsonResponse.tracks) {
+         return [];
+        }
+        return jsonResponse.tracks.items.map((track) => ({
+          id: track.id,
+          name: track.name,
+          artist: track.artists[0].name,
+          album: track.album.name,
+          uri: track.uri,
+        }));
+      });
+  },
+};
 
 export default Spotify;
